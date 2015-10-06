ad7d93947ed62830f6b605bbed2d9165c9d3bb2c
Kendrick <kbesnard@lefigaro.fr>
2015-10-06 10:03:02 +0200

    At Internet Xiti init

---
+++ libraries/atinternet/xiti/v1/Tag.js
@@ -0,0 +1,32 @@
+//:include tagsdk-current.js
+
+qubit.opentag.LibraryTag.define("atinternet.xiti.v1.Tag", {
+	config: {
+		/*DATA*/
+		name: "xiti",
+		async: false,
+		description: "",
+		html: "",
+		locationDetail: "",
+		isPrivate: false,
+		url: "",
+		usesDocWrite: false,
+		upgradeable: true,
+		parameters: [
+
+	]
+		/*~DATA*/
+	},
+	script: function() {
+	/*SCRIPT*/
+	/*~SCRIPT*/
+	},
+	pre: function() {
+	/*PRE*/
+	/*~PRE*/
+	},
+	post: function() {
+	/*POST*/
+	/*~POST*/
+	}
+});
